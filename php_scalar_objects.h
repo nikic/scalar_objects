a94d1ba9333425db900c1c1f3adb54e7ac025335
Nikita Popov <nikic@php.net>
2014-11-26 23:35:11 +0100

    Remove PHP license stubs
    PHP license for exts doesn't even make sense...

--- php_scalar_objects.h
+++ php_scalar_objects.h
@@ -1,23 +1,3 @@
-/*
-  +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2013 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author:                                                              |
-  +----------------------------------------------------------------------+
-*/
-
-/* $Id$ */
-
 #ifndef PHP_SCALAR_OBJECTS_H
 #define PHP_SCALAR_OBJECTS_H
 
@@ -73,3 +53,4 @@ ZEND_END_MODULE_GLOBALS(scalar_objects)
  * vim600: noet sw=4 ts=4 fdm=marker
  * vim<600: noet sw=4 ts=4
  */
+
