ad1d050b3987a76a62e524a412c6608c8fe4f6ad
Nikita Popov <nikita.ppv@gmail.com>
2022-08-28 12:16:58 +0200

    Try to fix appveyor build
    This just copies the install.ps1 from apcu

--- .appveyor/install.ps1
+++ .appveyor/install.ps1
@@ -1,64 +1,61 @@
 $ErrorActionPreference = "Stop"
 
-if (-not (Test-Path 'C:\build-cache')) {
-    [void](New-Item 'C:\build-cache' -ItemType 'directory')
+if (-not (Test-Path 'c:\build-cache')) {
+    [void](New-Item 'c:\build-cache' -ItemType 'directory')
 }
 
 $bname = "php-sdk-$env:BIN_SDK_VER.zip"
-if (-not (Test-Path "C:\build-cache\$bname")) {
-    Invoke-WebRequest "https://github.com/Microsoft/php-sdk-binary-tools/archive/$bname" -OutFile "C:\build-cache\$bname"
+Write-Host $bname
+if (-not (Test-Path c:\build-cache\$bname)) {
+    Invoke-WebRequest "https://github.com/microsoft/php-sdk-binary-tools/archive/$bname" -OutFile "c:\build-cache\$bname"
 }
 $dname0 = "php-sdk-binary-tools-php-sdk-$env:BIN_SDK_VER"
 $dname1 = "php-sdk-$env:BIN_SDK_VER"
-if (-not (Test-Path "C:\build-cache\$dname1")) {
-    Expand-Archive "C:\build-cache\$bname" 'C:\build-cache'
-    Move-Item "C:\build-cache\$dname0" "C:\build-cache\$dname1"
+if (-not (Test-Path 'c:\build-cache\$dname1')) {
+    Expand-Archive "c:\build-cache\$bname" "c:\build-cache"
+    Move-Item "c:\build-cache\$dname0" "c:\build-cache\$dname1"
 }
 
-$gareleases = Invoke-WebRequest "https://windows.php.net/downloads/releases/releases.json" | ConvertFrom-Json
-$qareleases = Invoke-WebRequest "https://windows.php.net/downloads/qa/releases.json" | ConvertFrom-Json
-$garev = [regex]::split($gareleases.$env:PHP_VER.version, '[^\d]')[2]
-$qarev = [regex]::split($qareleases.$env:PHP_VER.version, '[^\d]')[2]
-if ($qarev -gt $garev) {
-    $phpversion = $qareleases.$env:PHP_VER.version
-    $phprelease = 'QA'
+$releases = @{
+    '7.0' = '7.0.33';
+    '7.1' = '7.1.33';
+    '7.2' = '7.2.34';
+    '7.3' = '7.3.33';
+}
+if ($releases.ContainsKey($env:PHP_VER)) {
+    $phpversion = $releases.$env:PHP_VER;
+    $base_url = 'http://windows.php.net/downloads/releases/archives';
 } else {
-    $phpversion = $gareleases.$env:PHP_VER.version
-    $phprelease = 'GA'
+    $releases = Invoke-WebRequest https://windows.php.net/downloads/releases/releases.json | ConvertFrom-Json
+    $phpversion = $releases.$env:PHP_VER.version
+    $base_url = 'http://windows.php.net/downloads/releases';
 }
 
 $ts_part = ''
 if ($env:TS -eq '0') {
     $ts_part += '-nts'
 }
+
 $bname = "php-devel-pack-$phpversion$ts_part-Win32-$env:VC-$env:ARCH.zip"
-if (-not (Test-Path "C:\build-cache\$bname")) {
-    if ($phprelease -eq "GA") {
-        Invoke-WebRequest "https://windows.php.net/downloads/releases/$bname" -OutFile "C:\build-cache\$bname"
-    } else {
-        Invoke-WebRequest "https://windows.php.net/downloads/qa/$bname" -OutFile "C:\build-cache\$bname"
-    }
+if (-not (Test-Path "c:\build-cache\$bname")) {
+    Invoke-WebRequest "$base_url/$bname" -OutFile "c:\build-cache\$bname"
 }
 $dname0 = "php-$phpversion-devel-$env:VC-$env:ARCH"
 $dname1 = "php-$phpversion$ts_part-devel-$env:VC-$env:ARCH"
-if (-not (Test-Path "C:\build-cache\$dname1")) {
-    Expand-Archive "C:\build-cache\$bname" 'C:\build-cache'
+if (-not (Test-Path "c:\build-cache\$dname1")) {
+    Expand-Archive "c:\build-cache\$bname" "c:\build-cache"
     if ($dname0 -ne $dname1) {
-        Move-Item "C:\build-cache\$dname0" "C:\build-cache\$dname1"
+        Move-Item "c:\build-cache\$dname0" "c:\build-cache\$dname1"
     }
 }
-$env:PATH = "C:\build-cache\$dname1;$env:PATH"
+$env:PATH = "c:\build-cache\$dname1;$env:PATH"
 
 $bname = "php-$phpversion$ts_part-Win32-$env:VC-$env:ARCH.zip"
-if (-not (Test-Path "C:\build-cache\$bname")) {
-    if ($phprelease -eq "GA") {
-        Invoke-WebRequest "https://windows.php.net/downloads/releases/$bname" -OutFile "C:\build-cache\$bname"
-    } else {
-        Invoke-WebRequest "https://windows.php.net/downloads/qa/$bname" -OutFile "C:\build-cache\$bname"
-    }
+if (-not (Test-Path "c:\build-cache\$bname")) {
+    Invoke-WebRequest "$base_url/$bname" -OutFile "c:\build-cache\$bname"
 }
 $dname = "php-$phpversion$ts_part-$env:VC-$env:ARCH"
-if (-not (Test-Path "C:\build-cache\$dname")) {
-    Expand-Archive "C:\build-cache\$bname" "C:\build-cache\$dname"
+if (-not (Test-Path "c:\build-cache\$dname")) {
+    Expand-Archive "c:\build-cache\$bname" "c:\build-cache\$dname"
 }
 $env:PATH = "c:\build-cache\$dname;$env:PATH"
